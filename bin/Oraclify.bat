f64b3ace00c9663b892999648ffba1de10441ca7
unknown <Jamie@JBuckleyDesktop.qdomain.local>
2015-08-18 13:31:05 +0100

    Handles spaces in file paths

--- bin/Oraclify.bat
+++ bin/Oraclify.bat
@@ -1,3 +1,6 @@
+echo "param 1 is " %1
+
 SET ThisScriptsDirectory=%~dp0
 SET PowerShellScriptPath=%ThisScriptsDirectory%Oraclify.ps1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'" %1
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'" '%1' 
+pause
